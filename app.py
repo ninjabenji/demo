ec29d0680e3e96c90dc8ca36a3635fe9fa5f1f8e
ninjabenji <76439609+ninjabenji@users.noreply.github.com>
2021-06-22 18:01:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,22 @@
-#App can use docker "config" (simple values) to obtain local syslog server name/port
-
 import logging, logging.handlers, time
 from get_docker_secret import get_docker_secret
 
-syslog_host = get_docker_secret('global_syslog_host', default='localhost')
-syslog_port = get_docker_secret('global_syslog_port', default='514')
-
-# create logger
 logger = logging.getLogger('App-Name')
 logger.setLevel(logging.DEBUG)
 
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+syslog_host = get_docker_secret('global_syslog_host', default='localhost')
+syslog_port = get_docker_secret('global_syslog_port', default='514', cast_to=int)
+hostname = get_docker_secret('hostname', default='hostname')
 
+formatter = logging.Formatter(f'%(asctime)s {hostname}[%(process)d]: %(name)s - %(levelname)s - %(message)s\n')
+formatter.default_time_format='%Y-%m-%dT%H:%M:%SZ'
+formatter.default_msec_format=None
+formatter.converter=time.gmtime
 sl = logging.handlers.SysLogHandler(address=(syslog_host, syslog_port))
-
-# create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s\n')
-
-# add formatter to ch
-ch.setFormatter(formatter)
+#sl.ident = 'containerid'
 sl.setFormatter(formatter)
-
-# add ch to logger
-#logger.addHandler(ch)
 logger.addHandler(sl)
 
-# 'application' code
-logger.debug('debug message')
-logger.info('info message')
-logger.warning('warn message')
-logger.error('error message')
-logger.critical('critical message')
-
-
-
 while True:
-	time.sleep(5)
-	logger.info('Something Interesting Happened')
+    time.sleep(5)
+    logger.info('Something Interesting Happened')
